676b61edc421c5decda200677c33d9b1895004c0
Liam Naylor <liam@liamnaylor.com>
2022-09-22 16:14:04 -0700

    Comparing arrays

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  else {
+    console.log(`🛑 Assertion Failed: ${actual} != ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  let matching = false;
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        matching = true;
+      }
+      else {
+        matching = false;
+      }
+    }
+    return matching;
+  }
+  else {
+    return matching;
+  }
+}
+assertEqual((eqArrays(["1", "2", "3"], ["1", "2", "3"])), true); // => true
+assertEqual((eqArrays(["1", "2", "3"], ["1", "2", 3])), false); // => true
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
