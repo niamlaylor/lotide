4e89db8ec87e48f996ac88c36f479c2a8acb5ce0
Liam Naylor <liam@liamnaylor.com>
2022-09-23 14:47:54 -0700

    Function to flatten an array

---
+++ flatten.js
@@ -0,0 +1,45 @@
+const eqArrays = function(arr1, arr2) {
+  let matching = false;
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        matching = true;
+      }
+      else {
+        matching = false;
+        break;
+      }
+    }
+    return matching;
+  }
+  else {
+    return matching;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  let checkForMatch = eqArrays(array1, array2);
+  if (checkForMatch) {
+    console.log(`✅ Assertion Passed: ${array1} === ${array2}`)
+  }
+  else if (!checkForMatch) {
+    console.log(`🛑 Assertion Failed: ${array1} != ${array2}`)
+  }
+};
+
+const flatten = function(arrayToFlatten) {
+  let result = [];
+  for (let i = 0; i < arrayToFlatten.length; i++) {
+    const value = arrayToFlatten[i];
+    if (!Array.isArray(value)) {
+      console.log(value);
+      result.push(value);
+    }
+    else if (Array.isArray(value)) {
+      for (let x = 0; x < value.length; x++) {
+        result.push(value[x]);
+      }
+    }
+  } return result;
+}
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [ 1, 2, 3, 4, 5, 6 ]);
