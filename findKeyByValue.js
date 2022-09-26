5d12ed67f27412f948ff7716f5e968fe5ea04d4a
Liam Naylor <liam@liamnaylor.com>
2022-09-25 19:05:19 -0700

    Initial commit: function to return key of a value

---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  else {
+    console.log(`🛑 Assertion Failed: ${actual} != ${expected}`);
+  }
+};
+
+const findKeyByValue = function(objectToCheck, value) {
+  const keys = Object.keys(objectToCheck);
+  let matchProperty = undefined;
+
+  for (let p = 0; p < keys.length; p++) {
+    if (value === objectToCheck[keys[p]]) {
+      matchProperty = keys[p];
+    }
+  } return matchProperty;
+};
+
+
+const bestTVShowsByGenre = {
+  sci_fi: 'The Expanse',
+  comedy: 'Brooklyn Nine-Nine',
+  drama: 'The Wire',
+  horror: 'Haunting of Hill House',
+  crime: 'Blue Blood'
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), 'sci_fi');
