c04c084bc2664f87fc2dde419d6416998353af42
Liam Naylor <liam@liamnaylor.com>
2022-10-10 17:28:39 -0700

    Refactored and made modular

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,25 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  let matching = false;
-  if ((arr1.length === arr2.length) && arr1.length > 0) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-        matching = true;
-      }
-      else {
-        matching = false;
-        break;
-      }
-    }
-    return matching;
-  }
-  else if ((arr1.length === arr2.length) && arr1.length === 0){
-    matching = true;
-  }
-  else {
-    return matching;
-  }
-  return matching;
-}
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
   let checkForMatch = eqArrays(array1, array2);
@@ -30,6 +9,5 @@ const assertArraysEqual = function(array1, array2) {
     console.log(`🛑 Assertion Failed: ${array1} != ${array2}`)
   }
 };
-assertArraysEqual([], []);
 
 module.exports = assertArraysEqual;
