17fe5b6cab8bf174b8d678de73268a588737fb7c
Liam Naylor <liam@liamnaylor.com>
2022-10-11 16:17:43 -0700

    Removing test cases

--- without.js
+++ without.js
@@ -1,36 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  let matching = false;
-  if ((arr1.length === arr2.length) && arr1.length > 0) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-        matching = true;
-      }
-      else {
-        matching = false;
-        break;
-      }
-    }
-    return matching;
-  }
-  else if ((arr1.length === arr2.length) && arr1.length === 0){
-    matching = true;
-  }
-  else {
-    return matching;
-  }
-  return matching;
-}
-
-const assertArraysEqual = function(array1, array2) {
-  let checkForMatch = eqArrays(array1, array2);
-  if (checkForMatch) {
-    console.log(`✅ Assertion Passed: ${array1} === ${array2}`)
-  }
-  else if (!checkForMatch) {
-    console.log(`🛑 Assertion Failed: ${array1} != ${array2}`)
-  }
-};
-
 const without = function(source, itemsToRemove) {
   let result = [];
   for (let i = 0; i < source.length; i++) {
