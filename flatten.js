94214c4ac1deeffa14294b83024568d743829fa8
Liam Naylor <liam@liamnaylor.com>
2022-10-12 15:11:45 -0700

    Refactor and added chai test cases

--- flatten.js
+++ flatten.js
@@ -1,17 +1,18 @@
+// refactored with recursion
+
 const flatten = function(arrayToFlatten) {
   let result = [];
-  for (let i = 0; i < arrayToFlatten.length; i++) {
-    const value = arrayToFlatten[i];
-    if (!Array.isArray(value)) {
-      result.push(value);
+  arrayToFlatten.forEach((item) => {
+    if (Array.isArray(item)) {
+      result = result.concat(flatten(item));
+    } else {
+      result.push(item);
     }
-    else if (Array.isArray(value)) {
-      for (let x = 0; x < value.length; x++) {
-        result.push(value[x]);
-      }
-    }
-  } return result;
-}
-//assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [ 1, 2, 3, 4, 5, 6 ]);
+  });
+  return result;
+};
+
+// console.log(flatten([1, 2, [3, 4], 5, [6]]));
+// expect [1, 2, 3, 4, 5, 6]
 
 module.exports = flatten;
